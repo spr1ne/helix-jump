6e86c37b933143f0c7f9db551a34bda800f2c6df
r.dampilon <dampilon.robert@gmail.com>
2025-03-24 12:37:11 +0700

    changed base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "/helix-jump/",
   plugins: [
     wasm(),
     topLevelAwait()
